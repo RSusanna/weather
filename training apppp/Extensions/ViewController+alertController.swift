7bbb063d3437701dcd18d256bf45465cf2126fbf
Susanna R <susanna1235@gmail.com>
2023-07-30 22:53:52 +0300

    прописала логику для алерт контроллера

--- training apppp/Extensions/ViewController+alertController.swift
+++ training apppp/Extensions/ViewController+alertController.swift
@@ -5,4 +5,32 @@
 //  Created by Susanna R on 30.07.2023.
 //
 
-import Foundation
+import UIKit
+
+//Логика для алерт контроллера
+func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+    
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+    alertController.addTextField {
+       tf in
+        let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
+        tf.placeholder = cities.randomElement()
+    }
+    
+    let search = UIAlertAction(title: "Search", style: .default) {
+        action in
+        let textField = alertController.textFields?.first
+        guard let cityName = textField?.text else {
+            return
+        }
+        if cityName != "" {
+            print("search info for the \(cityName)")
+        }
+    }
+    let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+    alertController.addAction(search)
+    alertController.addAction(cancel)
+    
+}
+
